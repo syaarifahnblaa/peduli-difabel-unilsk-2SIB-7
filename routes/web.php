65b6c1edd9484f9da93eb3ccdc552db4700b7616
shafiyurahman <shafiyurahman22si.mahasiswa.pcr.ac.id>
2023-12-04 07:52:52 +0700

    ok

--- routes/web.php
+++ routes/web.php
@@ -2,17 +2,26 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+
+
+
 
 Route::get('/', function () {
     return view('welcome');
 });
+Route::resource('\user',\App\Http\Controllers\UserController::class);
+
+Route::resource('\seminar', \App\Http\Controllers\SeminarController::class);
+
+// Route::get('layout', function(){
+//     return view('layouts.main');
+// });
+// // Route::get('jquery', function(){
+// //     return view('jquery.latihan1');
+// // });
+//Auth::routes();
+
+//Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+//Route::get('/', function(){return view('welcome');
+
+
